e425abda324f26df23a207074579847ec4438f13
Baek JongChan <abcwe2209@naver.com>
2020-06-19 02:55:22 +0900

    scoremanagementsystem의 sort기능 rank별로 sort기능 구현

--- score_management_system.py
+++ score_management_system.py
@@ -1,5 +1,7 @@
 from score import Score
 
+def key_rank(item):
+    return item[1].sum
 class ScoreManagementSystem:
     def __init__(self):
         self._score={}
@@ -17,3 +19,44 @@ class ScoreManagementSystem:
             num += 1
             self._score[num] = Score(line.strip())
         return len(self._score)
+
+    def _make_cars_string(self, score,order_way):
+        result = ""
+        if order_way== 'asc':
+            num =1
+        elif order_way=='des':
+            num = len(score)
+        for key, item in score:
+            result = result + str(item.no) +','
+            result = result + item.name +','
+            result = result +str(int(item.kor)) +','
+            result = result + str(int(item.eng)) +','
+            result = result + str(int(item.math)) + ','
+            result = result + str(int(item.sum)) + ','
+            if item.avg % 1 >0 :
+                result = result + str(round(item.avg,2)) + ','
+            elif item.avg % 1 ==0:
+                result = result + str(int(item.avg)) + ','
+            if order_way== 'asc':
+                result = result + str(num) + '\n'
+                num +=1
+            elif order_way== 'des':
+                result = result + str(num) + '\n'
+                num -=1
+
+        return result.strip()
+    
+
+
+    def sort(self, order_key="register", order_way = "asc"):
+        if order_key == "register" and order_way == "asc":
+            sorted_score = sorted(self._score.items())
+        elif order_key == "register" and order_way == "des":
+            sorted_score = sorted(self._score.items() , reverse = True)
+        elif order_key == "rank" and order_way == "asc":
+            sorted_score = sorted(self._score.items(),key = key_rank, reverse = True)
+        elif order_key == "rank" and order_way == "des":
+            sorted_score = sorted(self._score.items(),key = key_rank )
+
+        result = self._make_cars_string(sorted_score,order_way)
+        return result
--- test_score_management_system.py
+++ test_score_management_system.py
@@ -42,6 +42,68 @@ class TestScoreManagementSystem(unittest.TestCase):
 
         self.m_open3.assert_called_with('score.csv', 'rt', encoding='utf-8')
 
+    def test_sort_1(self):
+
+        with patch("score_management_system.open",self.m_open1):
+            sms = ScoreManagementSystem()
+            sms.read('score.csv')
+
+            result = sms.sort(order_key="register",order_way="asc")
+            self.assertEqual('1,강호민,85,90,95,270,90,1',result)
+
+    def test_sort_2(self):
+
+        with patch("score_management_system.open",self.m_open2):
+            sms = ScoreManagementSystem()
+            sms.read('score.csv')
+
+            result = sms.sort(order_key="register",order_way="asc")
+            self.assertEqual('1,강호민,85,90,95,270,90,1\n2,김광호,80,70,60,210,70,2',result)
+
+    def test_sort_3(self):
+
+        with patch("score_management_system.open",self.m_open2):
+            sms = ScoreManagementSystem()
+            sms.read('score.csv')
+
+            result = sms.sort(order_key="register",order_way="des")
+            self.assertEqual('2,김광호,80,70,60,210,70,2\n1,강호민,85,90,95,270,90,1',result)
+
+    def test_sort_4(self):
+
+        with patch("score_management_system.open",self.m_open3):
+            sms = ScoreManagementSystem()
+            sms.read('score.csv')
+
+            result = sms.sort(order_key="register",order_way="des")
+            self.assertEqual('14,이주경,70,70,80,220,73.33,3\n2,김광호,80,70,60,210,70,2\n1,강호민,85,90,95,270,90,1',result)
+
+    def test_sort_5(self):
+
+        with patch("score_management_system.open",self.m_open3):
+            sms = ScoreManagementSystem()
+            sms.read('score.csv')
+
+            result = sms.sort(order_key="rank",order_way="asc")
+            self.assertEqual('1,강호민,85,90,95,270,90,1\n14,이주경,70,70,80,220,73.33,2\n2,김광호,80,70,60,210,70,3',result)
+
+    def test_sort_6(self):
+
+        with patch("score_management_system.open",self.m_open3):
+            sms = ScoreManagementSystem()
+            sms.read('score.csv')
+
+            result = sms.sort("rank","asc")
+            self.assertEqual('1,강호민,85,90,95,270,90,1\n14,이주경,70,70,80,220,73.33,2\n2,김광호,80,70,60,210,70,3',result)
+
+    def test_sort_7(self):
+
+        with patch("score_management_system.open",self.m_open3):
+            sms = ScoreManagementSystem()
+            sms.read('score.csv')
+
+            result = sms.sort("rank","des")
+            self.assertEqual('2,김광호,80,70,60,210,70,3\n14,이주경,70,70,80,220,73.33,2\n1,강호민,85,90,95,270,90,1',result)
 
 
 if __name__ == "__main__":
